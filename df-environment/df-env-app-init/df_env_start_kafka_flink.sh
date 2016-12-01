c898415485288708603225212a20775f1ceb30b7
Will <willddy@gmail.com>
2016-12-01 11:39:54 -0500

    add more scripts

--- df-environment/df-env-app-init/df_env_start_kafka_flink.sh
+++ df-environment/df-env-app-init/df_env_start_kafka_flink.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# Starting Hadoop
+if [ -h /opt/hadoop ]; then
+    echo "Starting Hadoop"
+    hadoop-daemon.sh start namenode
+    hadoop-daemon.sh start datanode
+fi
+
 # Starting Confluent Platform
 if [ -h /opt/confluent ]; then
     echo "Starting Confluent Kafka"
@@ -8,14 +15,22 @@ if [ -h /opt/confluent ]; then
     sleep 5
     kafka-server-start /mnt/etc/server.properties 1>> /mnt/logs/kafka.log 2>> /mnt/logs/kafka.log &
     sleep 5
-    #schema-registry-start /mnt/etc/schema-registry.properties 1>> /mnt/logs/schema-registry.log 2>> /mnt/logs/schema-registry.log &
-    #sleep 5
+    schema-registry-start /mnt/etc/schema-registry.properties 1>> /mnt/logs/schema-registry.log 2>> /mnt/logs/schema-registry.log &
+    sleep 5
 fi
 
 # Starting Flink
 if [ -h /opt/flink ]; then
     echo "Starting Apache Flink"
     /opt/flink/bin/start-cluster.sh
+fi
+
+# Starting Hive Metastore and server2
+if [ -h /opt/hive ]; then
+    echo "Starting Hive Metastore"
+    hive --service metastore 1>> /mnt/logs/metastore.log 2>> /mnt/logs/metastore.log &
+	echo "Starting Hive Server2"
+    hive --service hiveserver2 1>> /mnt/logs/hiveserver2.log 2>> /mnt/logs/hiveserver2.log &
 fi
 
 # Start Kafka Connect
