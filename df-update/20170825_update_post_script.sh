699f290788a068a5d5df62bf6cd39e180b374cd6
DataFibers Admin <datafibers@gmail.com>
2017-08-25 22:50:04 -0400

    Update 20170825_update_post_script.sh

--- df-update/20170825_update_post_script.sh
+++ df-update/20170825_update_post_script.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -e
-echo "TEST, THANK YOU for using DF update."
-echo "To apply the conf update, run dfops or df_ops install"
+echo "Post update script will add df_ops and dfops alias to ~.profile."
+sed -i '/alias df_ops/d' ~/.profile
+sed -i '/alias dfops/d' ~/.profile
+echo "alias df_ops='df_ops.sh'" >> ~/.profile
+echo "alias dfops='df_ops.sh'" >> ~/.profile
+source ~/.profile
+echo "Post update script completed."
